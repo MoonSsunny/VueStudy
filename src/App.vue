99703d1f0b8b478e4ea6139c52a532785b13b6dc
MoonSsunny <moonsun116@naver.com>
2021-08-24 12:12:20 +0900

    first

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <h1>직방</h1>
+    <div>
+      <h2>xx원룸</h2>
+      <p>{{ price1 }}만원</p>
+    </div>
+    <div>
+      <h2>xx원룸</h2>
+      <p>{{ price2 }}만원</p>
+    </div>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+// 데이터바인딩 이유
 
+<script>
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      price1: 200,
+      price2: 30,
+    };
+  },
+  components: {},
+};
 </script>
 
 <style>
